013ca514554b38aa3177edebe924e8e3cd4e2cae
Loic <konanloic10@gmail.com>
2021-08-25 00:47:22 -0500

    README.md

---
+++ Assignments/p10055/main.cpp
@@ -0,0 +1,24 @@
+/**
+* Loic Konan
+* 4883 - Programming Techniques
+* 08/29/2021
+*/
+
+#include <iostream>
+
+#define endl "\n"
+
+using namespace std;
+
+int main() 
+{
+    int B = 0, A = 0;
+    
+    while (cin >> A >> B) 
+    {
+        
+        // your stuff here
+
+    }
+    return 0;
+}
