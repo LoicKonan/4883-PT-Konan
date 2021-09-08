54f6e29aced8f7e3c2d73bbba19990005d1c2ba4
Loic <konanloic10@gmail.com>
2021-09-07 19:59:43 -0500

    README.md

---
+++ Assignments/A05/P161/main.cpp
@@ -0,0 +1,21 @@
+/**
+* Loic Konan
+* 4883 - Programming Techniques.
+* P161
+* 08/29/2021
+*/
+
+#include <iostream>
+using namespace std;
+
+// This gave me a run time of  seconds.
+int main() 
+{
+    long long A, B;
+    
+    while (cin >> A >> B) 
+    {
+      cout << abs(A-B) << '\n';
+    }
+    return 0;
+}
