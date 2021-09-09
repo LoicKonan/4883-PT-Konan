d62ef1b53effcf361031fc2f3ce1c1eb5d44c0ea
Loic <konanloic10@gmail.com>
2021-09-08 23:36:02 -0500

    README.md

---
+++ Assignments/A06/P10038/main.cpp
@@ -0,0 +1,24 @@
+/**
+* Loic Konan
+* 4883 - Programming Techniques.
+* P10038
+* 09/7/2021
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  int Time;    // Declare the variable to be use.
+  cin >> Time; // Read in each individual time in.
+
+  while (Time) // While our Time is not 0.
+  {
+  }
+
+  
+return 0;
+}
