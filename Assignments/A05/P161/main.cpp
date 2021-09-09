64cb9fab0449bd3043b08bd67f189d3c253ae988
Loic <konanloic10@gmail.com>
2021-09-08 22:53:49 -0500

    README.md

--- Assignments/A05/P161/main.cpp
+++ Assignments/A05/P161/main.cpp
@@ -20,7 +20,7 @@ int main()
   int Time;    // Declare the variable to be use.
   cin >> Time; // Read in each individual time in.
 
-  while (true) // While our Time is not 0.
+  while (Time) // While our Time is not 0.
   {
     vector<int> Series_Of_Integers; // Create a vector of integers that will hold our series of integers.
     while (Time)                    // While Time is not 0.
@@ -34,16 +34,16 @@ int main()
     // Find the time at which all lights synchronize or fail.
     // Finds the smallest element in the range of our vector multiply by 2.
     // and if it is less than and equal to 5 hours(which is 18000 seconds).
-    for (int j = (*min_element(Series_Of_Integers.begin(), Series_Of_Integers.end())) * 2; j <= 18000; j++)
+    for (int i = (*min_element(Series_Of_Integers.begin(), Series_Of_Integers.end())) * 2; i <= 18000; i++)
     {
       bool light_Green = true; // boolean for the green light.
 
       // Looping through our vector of integers.
-      for (int i = 0; i < Series_Of_Integers.size(); i++)
+      for (int j = 0; j < Series_Of_Integers.size(); j++)
       {
         // Let i be the waiting time for a light.
         // If (j % 2i) >= (i-5), then the light isn't green at time j.
-        if (j % (Series_Of_Integers[i] * 2) >= (Series_Of_Integers[i]) - 5)
+        if (i % (Series_Of_Integers[j] * 2) >= (Series_Of_Integers[j]) - 5)
         {
           light_Green = false; // The light is not green because the (Time % 2i) is not greater or equal to (i-5)
           break;
@@ -53,7 +53,7 @@ int main()
       if (light_Green) // if We have a green light.
       {                // The fail to synchronise will be false.
         fail_Sync = false;
-        TimeDisplay(j); // Now display the time format.
+        TimeDisplay(i); // Now display the time format.
         break;
       }
     }
@@ -64,8 +64,11 @@ int main()
     }
 
     cin >> Time; // Read in Time.
+    if (!Time)
+    {
+      cin >> Time;
+    }
   }
-  return 0;
 }
 
 void TimeDisplay(int seconds)
