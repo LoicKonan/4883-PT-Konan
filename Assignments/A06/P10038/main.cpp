e0a6ba6a85af2e0e89898c1fcea361523bf170e8
Loic <konanloic10@gmail.com>
2021-09-10 15:42:08 -0500

    README.md

--- Assignments/A06/P10038/main.cpp
+++ Assignments/A06/P10038/main.cpp
@@ -22,20 +22,18 @@ using namespace std;
 
 int main()
 {
-    int64 n_Input,         // Number of input per line <= 3000.
-          difference,      // absolutes values of the difference between successive elements.
-          first_Number,    // First integer(or the current integer).
-          second_Number,   // second integer(or the next or following integer).
-          array[3010];     // An array that will hold more than 3000 integers.
-                           // since we know that the Number of input per line <= 3000.
+    int64 n_Input,           // Number of input per line <= 3000.
+          difference,        // absolutes values of the difference between successive elements.
+          first_Number,      // First integer(or the current integer).
+          second_Number,     // second integer(or the next or following integer).
+          array[3010];       // An array that will hold more than 3000 integers.
+                             // since we know that the Number of input per line is <= 3000.
 
-    while (cin >> n_Input) // Read in the number of input as long as it > 0.
+    while (cin >> n_Input)   // Read in the number of input as long as it > 0.
     {
-        difference = 0;    // Initial it to zero.
-        second_Number = 0; // Initial it to zero.
-        bool check = false;// a boolean to check whenever it jolly or not.
-
-        REP(i, n_Input)    // Iterate through our inputs in a cool way.
+        bool failed = false; // a boolean to check whenever it jolly or not.
+ 
+        REP(i, n_Input)      // Iterate through our inputs in a cool way.
         {
             cin >> first_Number;                            // Read in our first number.
             difference = abs(first_Number - second_Number); // Store the absolute values of the difference bettween
@@ -46,19 +44,18 @@ int main()
             }
             second_Number = first_Number;  // Now store the value of the first number in the following number.
         }
-
         sort(array, array + n_Input - 1);                     // Sort our array
 
         for (int i = 1; i < n_Input - 1; i++)                 // set i to 1 then itterate through our inputs - 1.
         {                                                     // If my array does not ended up with all 1's,
             if (array[0] != 1 || array[i] - array[i - 1] != 1)// I don't have jolly jumper.
             {
-                check = true; // Since this is true, jump to the else and print not Jolly.
+                failed = true; // Since this is true, jump to the else and print not Jolly.
                 break;
             }
         }
-        if (!check) cout << "Jolly" << endl;                  // Print jolly.
-        else        cout << "Not jolly" << endl;              // Print not jolly.
+        if (!failed) cout << "Jolly" << endl;                 // Print jolly.
+        else         cout << "Not jolly" << endl;             // Print not jolly.
     }
     return 0;
 }
