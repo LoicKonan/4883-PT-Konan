fe0a9d0af0819897cfa272467eaed8f80cdff037
Loic <konanloic10@gmail.com>
2021-09-10 03:41:11 -0500

    README.md

--- Assignments/A06/P10038/main.cpp
+++ Assignments/A06/P10038/main.cpp
@@ -5,49 +5,58 @@
 * 09/9/2021
 */
 
-#include<iostream>
+#include <iostream>
 #include <algorithm>
 
 using namespace std;
 
+/**
+ * Credit to Dr.Griffin 
+ * Shortcut for my for loops and long long.
+ */
+
+#define REP(i, n) for (int i = 0; i < n; i++)  // simple 0=>N for
+#define int64 long long
+
 int main()
 {
-    int n_Input;        // Number of input per line <= 3000
-    int difference;     // absolutes values of the difference between successive elements 
-    int first_Number;   // 
-    int second_Number;  // 
-    int array[3005];    // An array that will hold more than 3000 integers
+    int64 n_Input,      // Number of input per line <= 3000.
+        difference,     // absolutes values of the difference between successive elements.
+        first_Number,   // First integer(or the current integer).
+        second_Number,  // second integer(or the next or following integer).
+        array[3005];    // An array that will hold more than 3000 integers.
                         // since we know that the Number of input per line <= 3000.
-    bool fail = false;  // a boolean to check whenever it jolly or not.
+    bool check = false; // a boolean to check whenever it jolly or not.
 
-    while(cin >> n_Input)   //
+    while (cin >> n_Input) // Read in the number of input as long as it > 0.
     {
-        second_Number = 0;  //
-        for(int i = 0; i < n_Input; i++)
+        second_Number = 0; // set the following number to 0;
+        REP (i,n_Input)    // itterate through our array in a cool way.
         {
-            cin >> first_Number;    //
-            difference = abs(first_Number - second_Number); 
-            if(i > 0)   //
+            cin >> first_Number;                            // Read in our first number.
+            difference = abs(first_Number - second_Number); // Store the absolute values of the first number
+                                                            // and the next number in that variable.
+            if (i > 0)
             {
-                array[i - 1] = difference;  //
-                cout << difference << " ";  // to debug. 
+                array[i - 1] = difference; // Store the difference to this element of our array.
+               // cout << difference << " "; // This is for Debuggin.
             }
-            second_Number = first_Number;   //
+            second_Number = first_Number; // set the value of the first number to the next following number.
         }
 
-        sort(array,(array + n_Input - 1));  //
+        sort(array, (array + n_Input - 1)); // Sort our array
 
-        for(int i = 1; i < n_Input - 1; i++)
-            {                                  //
-                if(array[i] - array[i-1] != 1||array[0] != 1)   
-                {
-                    fail = true;
-                    break;
-                }
+         REP (i,n_Input - 1)    // itterate through our array - 1.
+        {                                                      // If my array does not ended up with all 1's,
+            if (array[i] - array[i - 1] != 1 || array[0] != 1) // I don't have jolly jumper.
+            {
+                check = true; // Since this is true, jump to the else and print not Jolly.
+                break;
             }
-        if(!fail)                       //
+        }
+        if (!check) // Print jolly.
             cout << "Jolly\n";
-        else                            //
+        else // Print not jolly.
             cout << "not jolly\n";
     }
     return 0;
