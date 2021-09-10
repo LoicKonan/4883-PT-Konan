83a7726d2f6e8dacd0385db16c92edc89f47931d
Loic <konanloic10@gmail.com>
2021-09-10 04:33:07 -0500

    README.md

--- Assignments/A06/P10038/main.cpp
+++ Assignments/A06/P10038/main.cpp
@@ -12,52 +12,51 @@ using namespace std;
 
 /**
  * Credit to Dr.Griffin 
- * Shortcut for my for loops and long long.
+ * Shortcut the #define.
  */
 
-#define REP(i, n) for (int i = 0; i < n; i++)  // simple 0=>N for
+#define REP(i, n) for (int i = 0; i < n; i++) // simple 0=>N for
 #define int64 long long
 
 int main()
 {
-    int64 n_Input,      // Number of input per line <= 3000.
-        difference,     // absolutes values of the difference between successive elements.
-        first_Number,   // First integer(or the current integer).
-        second_Number,  // second integer(or the next or following integer).
-        array[3005];    // An array that will hold more than 3000 integers.
-                        // since we know that the Number of input per line <= 3000.
-    bool check = false; // a boolean to check whenever it jolly or not.
+    int64 n_Input,          // Number of input per line <= 3000.
+        difference,         // absolutes values of the difference between successive elements.
+        first_Number,       // First integer(or the current integer).
+        second_Number,      // second integer(or the next or following integer).
+        array[3005];        // An array that will hold more than 3000 integers.
+                            // since we know that the Number of input per line <= 3000.
 
     while (cin >> n_Input) // Read in the number of input as long as it > 0.
     {
-        second_Number = 0; // set the following number to 0;
-        REP (i,n_Input)    // itterate through our array in a cool way.
+        bool check = false; // a boolean to check whenever it jolly or not.
+
+        REP(i, n_Input)     // Iterate through our inputs in a cool way.
         {
             cin >> first_Number;                            // Read in our first number.
-            difference = abs(first_Number - second_Number); // Store the absolute values of the first number
-                                                            // and the next number in that variable.
-            if (i > 0)
+            difference = abs(first_Number - second_Number); // Store the absolute values of the difference bettween
+                                                            // the first number and the following number in this variable.
+            if (i > 0)                                      // when i from our loop is greater than 0,
             {
-                array[i - 1] = difference; // Store the difference to this element of our array.
-               // cout << difference << " "; // This is for Debuggin.
+                array[i - 1] = difference; // Store the value of the absolute value in this element of our array.
             }
-            second_Number = first_Number; // set the value of the first number to the next following number.
+            second_Number = first_Number; // Now store the value of the first number in the following number.
         }
 
-        sort(array, (array + n_Input - 1)); // Sort our array
+        sort(array, (array + n_Input - 1));                    // Sort our array
 
-         REP (i,n_Input - 1)    // itterate through our array - 1.
+        REP(i, n_Input - 1)                                    // itterate through our inputs - 1.
         {                                                      // If my array does not ended up with all 1's,
-            if (array[i] - array[i - 1] != 1 || array[0] != 1) // I don't have jolly jumper.
+            if (array[0] != 1 || array[i] - array[i - 1] != 1) // I don't have jolly jumper.
             {
                 check = true; // Since this is true, jump to the else and print not Jolly.
                 break;
             }
         }
-        if (!check) // Print jolly.
+        if (!check)                                             // Print jolly.
             cout << "Jolly\n";
-        else // Print not jolly.
-            cout << "not jolly\n";
+        else                                                    // Print not jolly.
+            cout << "Not jolly\n";
     }
     return 0;
 }
