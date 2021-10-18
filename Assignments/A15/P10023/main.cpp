920b4fbf3d8659dad2622b524d0c33f6eb4e2023
Loic <konanloic10@gmail.com>
2021-10-18 03:29:51 -0500

    README.md

--- Assignments/A15/P10023/main.cpp
+++ Assignments/A15/P10023/main.cpp
@@ -43,13 +43,16 @@ using namespace std;
 int main()
 {
     int test;
-    scanf("%d", &test);
-    
+    cin >> test;
+    // scanf("%d", &test);
+
     while (test--)
     {
         long double Y;
-        scanf("%lf", &Y);
-        printf("%.0lf\n", sqrtl(Y));
+        cin >> Y;
+        // scanf("%lf", &Y);
+        cout << Y << endl;
+        //printf("%.0lf\n", sqrtl(Y));
     }
     return 0;
 }
