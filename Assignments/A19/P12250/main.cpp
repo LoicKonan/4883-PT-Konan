5dbf5870180c6e8fd4e467b4f9ea28509c2a3e1e
Loic <konanloic10@gmail.com>
2021-10-22 00:21:57 -0500

    README.md

--- Assignments/A19/P12250/main.cpp
+++ Assignments/A19/P12250/main.cpp
@@ -34,11 +34,11 @@ typedef pair<int, int> ii; // pair of ints
 typedef vector<ii> vii;    // vector of pairs
 typedef vector<vi> vvi;    // vector of vector of ints
 
-int main(int argc, char **argv)
+int main()
 {
    
 
-    while (true)
+    while ()
     {
 
         
