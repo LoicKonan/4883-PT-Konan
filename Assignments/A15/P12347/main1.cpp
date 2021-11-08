393dce089c823eb671813241f8cd43e90eb7f7c1
Loic <konanloic10@gmail.com>
2021-11-07 22:40:19 -0600

    README.md

--- Assignments/A15/P12347/main1.cpp
+++ Assignments/A15/P12347/main1.cpp
@@ -1,41 +1,73 @@
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
 #include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+
+#define FileIn(file) freopen(file ".inp", "r", stdin)
+#define FileOut(file) freopen(file ".out", "w", stdout)
+#define FOR(i, a, b) for (int i = a; i <= b; i++)
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define Fill(ar, val) memset(ar, val, sizeof(ar))
+#define PI 3.1415926535897932385
+#define uint64 unsigned long long
+#define int64 long long
+#define all(ar) ar.begin(), ar.end()
+#define pb push_back
+#define bit(n) (1 << (n))
+#define Last(i) (i & -i)
+#define INF 500000000
+#define EPS 1e-7
+#define maxN 10000
 using namespace std;
+#define endl "\n"
 
-struct node
+/* A binary tree node has data, pointer to left child
+and a pointer to right child */
+struct Node
 {
-    int value;
-    node *left;
-    node *right;
-}
-
-class Binary_Search_Tree
-{
-public:
-    Binary_Search_Tree(); // Default constructor.
-    void insert(int key); // To insert a node.
-    void print();         // Print.
-    node *root;           // Pointer to root element.
-}
-
-Binary_Search_Tree::Binary_Search_Tree()
-{
-    root = NULL
-}
-
-void Binary_Search_Tree::insert(int key, node *left)
-{
-    if(key < leaf->value)
+    int data;
+    struct Node *left, *right;
+    Node(int data)
     {
-        if(leaf->left != NULL)
-        {
-            insert(key, leaf->left)
-        }
+        this->data = data;
+        left = right = NULL;
     }
-    else
-    {
-        root = new node;
-        root->value = key;
-        root->left = NULL;
-        root->right = NULL;
-    }
+};
+
+/* Given a binary tree, print its nodes in preorder*/
+void printPreorder(struct Node *node)
+{
+    if (node == NULL)
+        return;
+
+    /* first print data of node */
+    cout << node->data << " ";
+
+    /* then recur on left subtree */
+    printPreorder(node->left);
+
+    /* now recur on right subtree */
+    printPreorder(node->right);
+}
+
+/* Driver program to test above functions*/
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    cout << "\nPreorder traversal of binary tree is \n";
+    printPreorder(root);
+    return 0;
 }
