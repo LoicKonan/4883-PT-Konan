df0958f4bf5f016b1a0b3f8cad0f9a7c8f521827
Loic <konanloic10@gmail.com>
2021-12-07 10:33:44 -0600

    README.md

--- Assignments/A13/P1112/main.cpp
+++ Assignments/A13/P1112/main.cpp
@@ -35,12 +35,113 @@ typedef vector<ii> vii;    // vector of pairs
 typedef vector<vi> vvi;    // vector of vector of ints
 
 
-int main()
-{
+#define infinity 1 << 30
+#define mx 10000
 
-    while (true)
+vector<int> g[mx], cost[mx];
+
+struct node
+{
+    int u;
+    int cost;
+
+    node(int _u, int _cost)
     {
 
+        u = _u;
+
+        cost = _cost;
     }
-    return 0;
+
+    bool operator<(const node &p) const
+    {
+        return cost > p.cost;
+    }
+};
+
+
+void reset()
+{
+    for (int i = 0; i < mx; i++)
+
+    {
+        g[i].clear();
+
+        cost[i].clear();
+    }
+}
+
+void dijstkra(int n, vector<int> g[], vector<int> cost[], int source, int time, int k, int t)
+{
+    int distance[n + 1];
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        distance[i] = infinity;
+    }
+
+    priority_queue<node> q;
+
+    q.push(node(source, 0));
+    distance[source] = 0;
+
+    while (!q.empty())
+    {
+        node top = q.top();
+
+        q.pop();
+
+        int u = top.u;
+
+        for (int i = 0; i < (int)g[u].size(); i++)
+        {
+            int v = g[u][i];
+
+            if (distance[u] + cost[u][i] < distance[v])
+            {
+                distance[v] = distance[u] + cost[u][i];
+                q.push(node(v, distance[v]));
+            }
+        }
+    }
+
+    int cn = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (distance[i] <= time)
+            cn++;
+    }
+
+    cout << cn << endl;
+
+    if (k != t - 1)
+        cout << endl;
+}
+
+int main()
+{
+    int numNodes, numEdges, t, exit, time, k;
+
+    cin >> t;
+
+    for (k = 0; k < t; k++)
+    {
+        reset();
+
+        cin >> numNodes >> exit >> time >> numEdges;
+
+        for (int i = 0; i < numEdges; i++)
+        {
+            int u, v, w;
+            cin >> u >> v >> w;
+
+            g[u].push_back(v);
+            g[v].push_back(u);
+            cost[u].push_back(w);
+            cost[v].push_back(w);
+        }
+        dijstkra(numNodes, g, cost, exit, time, k, t);
+    }
 }
