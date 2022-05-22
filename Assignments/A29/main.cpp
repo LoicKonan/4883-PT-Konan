0afc015c68d14cea7839491c9b67bcb7121cf917
Loic <konanloic10@gmail.com>
2022-05-21 23:05:30 -0500

    README.md

---
+++ Assignments/A29/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int input, a, b;
+
+    while (cin >> input)
+    {
+        while (cin >> a >> b)
+        {
+
+        }
+    }
+    return 0;
+}
